22544f0ad69c41cbd2ceb48d127fa69fd0e7daf0
Marco Borba <marcoborba1702@gmail.com>
2024-10-10 20:28:40 -0300

    atualizado

--- Contexto/LocadoraAppDbContext.cs
+++ Contexto/LocadoraAppDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using LocadoraApp2.Classes;
 using Microsoft.EntityFrameworkCore;
 
 namespace LocadoraApp2.Contexto
