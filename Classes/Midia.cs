c180d65d0e0f7bd0bed4bea109efb8aa5ebd840c
Marco Borba <marcoborba1702@gmail.com>
2024-10-10 20:26:04 -0300

    criando  classe para Midia

---
+++ Classes/Midia.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LocadoraApp2.Classes
+{
+    internal class Midia
+    {
+        //Titulo
+        //Gênero
+        //Sinopse
+        //Autor
+        //Duração
+        //Classificação Indicativa
+
+        public int MidiaId { get; set; }
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public string Autor {  get; set; }
+        public string Sinopse { get; set; }
+        public int Ano  { get; set; }
+        public int Duracao { get; set; }
+        public string ClassicacaoIndicativa { get; set; }
+
+    }
+}
